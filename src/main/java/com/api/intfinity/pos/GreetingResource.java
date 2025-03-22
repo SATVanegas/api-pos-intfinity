5722502423e522a8e24fba50daf0d4912a9861b1
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2025-03-22 11:54:54 -0500

    Changed listening port

--- src/main/java/com/api/intfinity/pos/GreetingResource.java
+++ src/main/java/com/api/intfinity/pos/GreetingResource.java
@@ -3,6 +3,8 @@ package com.api.intfinity.pos;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
@@ -10,7 +12,13 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String hello(@Context HttpHeaders headers) {
+        // Log all headers
+        headers.getRequestHeaders().forEach((key, values) ->
+                System.out.println(key + ": " + values)
+        );
+
+        System.out.println("received request");
         return "Hello from Quarkus REST";
     }
 }
